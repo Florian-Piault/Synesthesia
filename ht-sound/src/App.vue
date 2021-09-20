d1a429acdbd98af2acfd219de41bb3047ece05f2
Florian PIAULT <florian.piault@mail-ecv.fr>
2021-09-20 16:51:02 +0200

    options (heights, effects)

--- ht-sound/src/App.vue
+++ ht-sound/src/App.vue
@@ -1,33 +1,62 @@
 <template>
   <div id="app">
     <h1>TONE.JS</h1>
-    <button @click="startAudio()"> Start </button>
+
+    <!-- HAUTEURS -->
+    <label>Hauteur:</label>
+    <select v-model="Height">
+      <option v-for="height in heights" :value="height" :key="height"> {{height}}</option>
+    </select>
+
+    <!-- EFFETS -->
+    <label>Effet:</label>
+    <select v-model="Effect">
+      <option v-for="effect in effects" :value="effect" :key="effect"> {{effect}}</option>
+    </select>
+
+    <div class="notes-panel">
+      <Note v-for="note in notes" :pitch="note + Height" :effect="effect" :key="note"></Note>
+    </div>
   </div>
 </template>
 
 <script>
-import * as Tone from 'tone';
+import Note from './components/Note.vue';
 
 export default {
-  data(){
-    return {
-      synth: null
-    }
-  },
   name: 'App',
   components: {
+    Note,
   },
-  mounted(){
-    this.synth = new Tone.Synth().toDestination();
-  },
-  methods: {
-    startAudio(){
-          this.synth.triggerAttackRelease("C1", "8n");
-
-      Tone.start();
+  data() {
+    return {
+        Height: '4',
+        Effect: 'none',
+        notes: [
+        'C',
+        'D',
+        'E',
+        'F',
+        'G',
+        'A',
+        'B',
+      ],
+      heights: [
+        '0',
+        '1',
+        '2',
+        '3',
+        '4',
+        '5',
+      ],
+      effects: [
+        'none',
+        'reverb',
+        'chorus',
+        'bitCrusher'
+      ]
     }
   }
-
 }
 </script>
 
@@ -40,4 +69,10 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+  .notes-panel {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
 </style>
--- ht-sound/src/components/Note.vue
+++ ht-sound/src/components/Note.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-      
+		<button @click="startAudio()">{{pitch}}</button>
   </div>
 </template>
 
 <script>
+import * as Tone from 'tone';
+
 export default {
+	props: ['pitch', 'effect'],
+	// EFFECTS : none / reverb / chorus / bitCrusher
+	methods: {
+		startAudio() {
+			const crusher = new Tone.BitCrusher(4).toDestination();
+			const synth = new Tone.Synth().connect(crusher);
+			synth.triggerAttackRelease(this.pitch, 2);
+			Tone.start();
+			// this.synth.triggerAttackRelease(this.pitch, "8n");
+			// Tone.start();
+		}
+	}
+
+
 
 }
 </script>
 
 <style>
-
+	button {
+		height:100px;
+		width: 100px;
+		margin: 0;
+		padding: 0;
+	}
 </style>
