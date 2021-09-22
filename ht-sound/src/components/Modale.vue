83cc299400ffe3795d385c9ceb5485c725891663
Laaurent <laurent-quenel@hotmail.Fr>
2021-09-22 14:13:43 +0200

    design modale

--- ht-sound/src/components/Modale.vue
+++ ht-sound/src/components/Modale.vue
@@ -1,52 +1,54 @@
 <template>
   <div class="modale-container">
-      <div class="btn-container">
-        <button class="close-modale" @click="closeModale()">
-            <img src="@/assets/close.svg" alt="close" class="close-modale">
-        </button>
-      </div>
-      <slot></slot>
+    <div class="btn-container">
+      <button class="close-modale button-modale" @click="closeModale()">
+        <img src="@/assets/close.svg" alt="close" class="close-modale" />
+      </button>
+    </div>
+    <slot></slot>
   </div>
 </template>
 
 <script>
 export default {
-    name: 'Modale',
-    methods: {
-        closeModale() {
-            this.$emit('closeModale');
-        }
-    }
-
-}
+  name: "Modale",
+  methods: {
+    closeModale() {
+      this.$emit("closeModale");
+    },
+  },
+};
 </script>
 
 <style scoped>
-    .btn-container {
-        width: 100%;
-        height: 64px;
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-    }
+  .btn-container {
+    width: 100%;
+    height: 64px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
 
-    .close-modale {
-        display: flex;
-        border: none;
-        background: none;
-        cursor: pointer;
-    }
+  .close-modale {
+    display: flex;
+    border: none;
+    background: none;
+    cursor: pointer;
+  }
 
-    .close-modale:hover {
-        filter: opacity(0.7)
-    }
+  .close-modale:hover {
+    filter: opacity(0.7);
+  }
 
-    .modale-container {
-        z-index: 10;
-        height:100vh;
-        width: 512px;
-        position: absolute;
-        background-color: rgba(200,200,200,0.9);
-        padding: 16px 32px;
-    }
+  .modale-container {
+    z-index: 10;
+    width: 512px;
+    height: 100vh;
+    position: absolute;
+    background-color: rgba(255, 255, 255, 0.3);
+    backdrop-filter: blur(7px);
+    padding: 0px 32px 0;
+    border-top-right-radius: 10px;
+    border-bottom-right-radius: 10px;
+  }
 </style>
